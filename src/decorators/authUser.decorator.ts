dff46fbd705e57bbf5d0efea3a8ddc9b24ab63f4
PedroBranda <pedrohbspg@gmail.com>
2023-06-22 00:14:12 -0300

    Adicionando types para filtragem de propriedades no decorator do AuthUser

--- src/decorators/authUser.decorator.ts
+++ src/decorators/authUser.decorator.ts
@@ -1,10 +1,24 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+type AuthUserPropertyTypes =
+  | 'id'
+  | 'permissions'
+  | 'firstName'
+  | 'lastName'
+  | 'email'
+  | 'password'
+  | 'createdAt'
+  | 'updatedAt'
+  | 'deletedAt'
+  | 'createdBy'
+  | 'updatedBy'
+  | 'deletedBy';
+
 export const AuthUser = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
+  (property: AuthUserPropertyTypes, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
 
-    return data ? user?.[data] : user;
+    return property ? user?.[property] : user;
   },
 );
